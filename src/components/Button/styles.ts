3ed843ed395059d5e1568d9114af301630adbb1d
João Colares <joaocolaresm@gmail.com>
2025-01-10 16:29:29 -0300

    aula_3

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -11,6 +11,7 @@ export const ButtonContainer = styled.button<Props>`
   font-weight: bold;
   padding: 8px 16px;
   border-radius: 8px;
+  cursor: pointer;
 `
 
 export const ButtonLink = styled(Link)`
@@ -22,4 +23,5 @@ export const ButtonLink = styled(Link)`
   padding: 8px 16px;
   text-decoration: none;
   border-radius: 8px;
+  cursor: pointer;
 `
